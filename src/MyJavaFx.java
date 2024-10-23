8724a3980d9a3f2d64f6abf46bd9737ef2a9a95d
asvattha <singhd1@sfasu.edu>
2024-10-23 11:24:04 -0500

    Added pane

--- src/MyJavaFx.java
+++ src/MyJavaFx.java
@@ -1,23 +1,22 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class MyJavaFx extends Application{
 
     @Override
     public void start(Stage primaryStage){
+
+        StackPane pane = new StackPane();
+
         Button btnOk = new Button("OK");
-        Scene scene = new Scene(btnOk, 200, 200);
+        pane.getChildren().add(btnOk);
+
+        Scene scene = new Scene(pane, 200, 200);
         primaryStage.setScene(scene);
         primaryStage.show();
-
-        // Creating custom stage
-        // Scenes and Buttons are not shared between stages
-        Stage myStage = new Stage();
-        myStage.setScene(new Scene(new Button("Cancel"), 200, 200));
-        myStage.show();
-        myStage.setResizable(false);
     }
     public static void main(String[] args) throws Exception {
         Application.launch(args);
